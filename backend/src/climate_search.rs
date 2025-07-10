6350558c6c66b16de3eb921d8b98c81c245916f3
Aleksei Berezkin <16083785+aleksei-berezkin@users.noreply.github.com>
2025-07-10 07:13:38 +0200

    Optimized filtering by min distance by using cartesian coords and chord len

--- backend/src/climate_search.rs
+++ backend/src/climate_search.rs
@@ -1,21 +1,22 @@
 use backend::{get_relative_minmax, minmax, reduce_minmax};
 use common::cities::City;
-use rayon::{prelude::*, result};
+use rayon::prelude::*;
 
 #[derive(Debug)]
 pub struct ClimateSearchItem<'a> {
     id: usize,
     city: &'a City,
-    coords: GeoCoordinates,
     relative_minmax: ClimateMinMax,
 }
 
-#[derive(Clone, Copy, Debug)]
-pub struct GeoCoordinates {
-    /// Latitude in radians
-    phi: f64,
-    /// Longitude in radians
-    lambda: f64,
+#[derive(Debug)]
+pub struct ClimateMinMax {
+    humidity: (f32, f32),
+    ppt: (f32, f32),
+    srad: (f32, f32),
+    tmax: (f32, f32),
+    tmin: (f32, f32),
+    ws: (f32, f32),
 }
 
 pub fn make_climate_search_items<'a>(cities: &'a Vec<City>) -> Vec<ClimateSearchItem<'a>> {
@@ -35,10 +36,6 @@ pub fn make_climate_search_items<'a>(cities: &'a Vec<City>) -> Vec<ClimateSearch
 
     let items = cities.par_iter().enumerate()
         .map(|(index, city)| {
-            let coords = GeoCoordinates {
-                phi: city.latitude.to_radians(),
-                lambda: city.longitude.to_radians(),
-            };
             let climate_minmax = get_climate_min_max(city);
             let relative_minmax = ClimateMinMax {
                 humidity: get_relative_minmax(climate_minmax.humidity, total_minmax.humidity),
@@ -51,26 +48,14 @@ pub fn make_climate_search_items<'a>(cities: &'a Vec<City>) -> Vec<ClimateSearch
             ClimateSearchItem {
                 id: index,
                 city,
-                coords,
                 relative_minmax,
             }
         })
         .collect();
 
-    // items.sort_by_key(|item| item.id);
     eprintln!("Built climate search items in {} ms", start.elapsed().as_millis());
 
     items
-}
-
-#[derive(Debug)]
-pub struct ClimateMinMax {
-    humidity: (f32, f32),
-    ppt: (f32, f32),
-    srad: (f32, f32),
-    tmax: (f32, f32),
-    tmin: (f32, f32),
-    ws: (f32, f32),
 }
 
 fn get_climate_min_max(city: &City) -> ClimateMinMax {
@@ -95,43 +80,79 @@ fn get_climate_min_max(city: &City) -> ClimateMinMax {
 pub struct ClimateScoredItem<'a> {
     id: usize,
     city: &'a City,
-    coords: GeoCoordinates,
+    /// Direct (chorde) distance is much faster than the precise arc distance
+    cartesian_xyz: [f64; 3],
     diff: f32,
-    distance_km: f64,
 }
 
-pub fn search_climate<'a>(items: &'a Vec<ClimateSearchItem>, query: &'a ClimateSearchItem) -> Vec<ClimateScoredItem<'a>> {
-    let min_dist = 200.0;
+#[derive(Debug)]
+pub struct ClimateResultItem<'a> {
+    id: usize,
+    city: &'a City,
+    distance_km: f64,
+    similarity_percent: f32,
+}
 
-    let mut scored_items = items.par_iter().enumerate()
-        .map(|(index, item)| ClimateScoredItem {
-            id: index,
-            city: item.city,
-            coords: item.coords,
-            diff: get_climate_diff(item, query),
-            distance_km: get_distance_km(&item.coords, &query.coords)
-        })
-        .filter(|item|
-            item.id == query.id || item.diff < 1.5 && item.distance_km >= min_dist
-        )
-        .collect::<Vec<_>>();
+pub fn search_climate<'a>(items: &'a Vec<ClimateSearchItem>, query: &'a ClimateSearchItem) -> Vec<ClimateResultItem<'a>> {
+    let min_chord_length = arc_length_to_chord_length(200.0);
+    let min_chord_length_sq = min_chord_length * min_chord_length;
 
-    // eprintln!("Found {} candidates", scored_items.len());
+    let (scored_items, max_diff) = score_items(items, query);
 
-    scored_items.par_sort_by(|a, b| a.diff.total_cmp(&b.diff));
+    eprintln!("Max diff {}", max_diff);
 
-    let mut result_items = Vec::<ClimateScoredItem>::new();
+    let mut filtered_items = Vec::<ClimateScoredItem>::new();
+
+    filtered_items.push(ClimateScoredItem {
+        id: query.id,
+        city: query.city,
+        cartesian_xyz: get_cartesian_xyz(query.city),
+        diff: 0.0,
+    });
+
     for item in scored_items {
-        if result_items.iter().all(|existing_res_it|
-            get_distance_km(&item.coords, &existing_res_it.coords) >= min_dist
+        // It's faster than spatial index (e.g. rstar)
+        if filtered_items.iter().all(|existing_res_it|
+            get_cartesian_distance_km_squared(&item.cartesian_xyz, &existing_res_it.cartesian_xyz) >= min_chord_length_sq
         ) {
-            result_items.push(item);
+            filtered_items.push(item);
         }
     }
 
-    // eprintln!("Found {} results", result_items.len());
+    eprintln!("Found {} results", filtered_items.len());
 
-    result_items.into_iter().take(10).collect()
+    filtered_items.into_iter()
+        .map(|item| ClimateResultItem {
+            id: item.id,
+            city: item.city,
+            distance_km: get_arc_distance_km(&item.city, &query.city),
+            similarity_percent: 100.0 * (1.0 - item.diff / max_diff),
+        })
+        .take(20)
+        .collect()
+}
+
+fn score_items<'a>(items: &'a Vec<ClimateSearchItem>, query: &'a ClimateSearchItem) -> (Vec<ClimateScoredItem<'a>>, f32) {
+    let scored_items = items.par_iter().enumerate()
+        .map(|(index, item)| ClimateScoredItem {
+            id: index,
+            city: item.city,
+            cartesian_xyz: get_cartesian_xyz(item.city),
+            diff: get_climate_diff(item, query),
+        })
+        .collect::<Vec<_>>();
+
+    let max_diff = scored_items.par_iter()
+        .max_by(|a, b| a.diff.total_cmp(&b.diff))
+        .unwrap().diff;
+
+    let mut scored_sorted = scored_items.into_par_iter()
+        .filter(|item| item.diff < max_diff / 2.0)
+        .collect::<Vec<_>>();
+    
+    scored_sorted.par_sort_by(|a, b| a.diff.total_cmp(&b.diff));
+
+    (scored_sorted, max_diff)
 }
 
 fn get_climate_diff(item: &ClimateSearchItem, query: &ClimateSearchItem) -> f32 {
@@ -151,12 +172,32 @@ fn diff_minmax(a: (f32, f32), b: (f32, f32)) -> f32 {
 
 const EARTH_RADIUS_KM: f64 = 6371.0;
 
-/// https://en.wikipedia.org/wiki/Great-circle_distance#Formulae
-fn get_distance_km(a: &GeoCoordinates, b: &GeoCoordinates) -> f64 {
-    EARTH_RADIUS_KM * (a.phi.sin() * b.phi.sin() + a.phi.cos() * b.phi.cos() * (a.lambda - b.lambda).abs().cos()).acos()
+/// https://en.wikipedia.org/wiki/Chord_(geometry)#In_trigonometry
+fn arc_length_to_chord_length(a: f64) -> f64 {
+    let theta = a / EARTH_RADIUS_KM;
+    EARTH_RADIUS_KM * 2.0 * (theta / 2.0).sin()
+}
 
-    // approx distance
-    // let dx = EARTH_RADIUS_KM * (a.lambda - b.lambda).abs() * ((a.phi + b.phi) / 2.0).cos();
-    // let dy = EARTH_RADIUS_KM * (a.phi - b.phi).abs();
-    // (dx * dx + dy * dy).sqrt()
+fn get_cartesian_xyz(city: &City) -> [f64; 3] {
+    [
+        EARTH_RADIUS_KM * city.latitude.to_radians().cos() * city.longitude.to_radians().cos(),
+        EARTH_RADIUS_KM * city.latitude.to_radians().cos() * city.longitude.to_radians().sin(),
+        EARTH_RADIUS_KM * city.latitude.to_radians().sin(),
+    ]
+}
+
+fn get_cartesian_distance_km_squared(a: &[f64; 3], b: &[f64; 3]) -> f64 {
+    let dx = a[0] - b[0];
+    let dy = a[1] - b[1];
+    let dz = a[2] - b[2];
+    dx * dx + dy * dy + dz * dz
+}
+
+/// https://en.wikipedia.org/wiki/Great-circle_distance#Formulae
+fn get_arc_distance_km(a: &City, b: &City) -> f64 {
+    let phi_a = a.latitude.to_radians();
+    let phi_b = b.latitude.to_radians();
+    let lambda_a = a.longitude.to_radians();
+    let lambda_b = b.longitude.to_radians();
+    EARTH_RADIUS_KM * (phi_a.sin() * phi_b.sin() + phi_a.cos() * phi_b.cos() * (lambda_a - lambda_b).abs().cos()).acos()
 }
