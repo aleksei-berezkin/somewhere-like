e0805825bbeb8e26823c8d540e0c9067f7d2cb1d
Aleksei Berezkin <16083785+aleksei-berezkin@users.noreply.github.com>
2025-07-24 07:09:23 +0200

    Font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: '400',
+})
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang='en' className={ roboto.className }>
       <body>{children}</body>
     </html>
   )
